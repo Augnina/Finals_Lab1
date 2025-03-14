f1de952f7cf750313965f23fc5bb2025467a5c0c
Augnina <apr1299@students.uc-bcf.edu.ph>
2025-03-14 08:43:58 +0800

    Add files via upload

---
+++ storage/framework/views/9f60627e66284b162cf6d9e83899739b.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+</head>
+<body>
+    <form action="<?php echo e(route('register')); ?>" method="POST">
+        <?php echo csrf_field(); ?>
+        <input type="text" name="name" placeholder="Name">
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="password" name="password" placeholder="Password">
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html><?php /**PATH C:\Users\jojol\Desktop\general\colej\year3\s2\cit18\Reburiano_Registration_Login\resources\views/auth/register.blade.php ENDPATH**/ ?>
